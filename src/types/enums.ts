fb906c9a5ca3467b3c61f80d1b99cd76c27fed46
augustofrade <aquaaticnaotrava@gmail.com>
2024-08-01 16:33:15 -0300

    Add tasks enums and interfaces

---
+++ src/types/enums.ts
@@ -0,0 +1,5 @@
+export enum Priority {
+    PRIORITY_1,
+    PRIORITY_2,
+    PRIORITY_3
+}
---
+++ src/types/interfaces.ts
@@ -0,0 +1,10 @@
+import { Priority } from "./enums";
+
+export interface Task {
+    title: string;
+    description: string;
+    priority: Priority;
+    date: Date;
+    labels: string[]; // TODO: add label
+    reminder: boolean;
+}
