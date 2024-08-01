c339ebd97dfa659ac29b2ab12a972cc67bf19645
augustofrade <aquaaticnaotrava@gmail.com>
2024-08-01 18:52:45 -0300

    Add tabs menu and capacitor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
-
-/* Core CSS required for Ionic components to work properly */
+import './theme/variables.css';
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
+import '@ionic/react/css/display.css';
+import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/palettes/dark.system.css';
+import '@ionic/react/css/structure.css';
 import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import '@ionic/react/css/typography.css';
 
+import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, setupIonicReact } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Redirect, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import TaskCreation from './pages/TaskCreation';
+import { todayOutline, calendarNumberOutline, personOutline } from 'ionicons/icons';
+
+/* Core CSS required for Ionic components to work properly */
+/* Basic CSS for apps built with Ionic */
+/* Optional CSS utils that can be commented out */
 /**
  * Ionic Dark Mode
  * -----------------------------------------------------
@@ -28,28 +31,45 @@ import '@ionic/react/css/display.css';
 
 /* import '@ionic/react/css/palettes/dark.always.css'; */
 /* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
-
 /* Theme variables */
-import './theme/variables.css';
-import TaskCreation from './pages/TaskCreation';
-
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-        <Route exact path="/task/new">
-          <TaskCreation />
-        </Route>
-      </IonRouterOutlet>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+          <Route exact path="/tasks/new">
+            <TaskCreation />
+          </Route>
+          { /* TODO: add routes to tasks and details pages */ }
+        </IonRouterOutlet>
+
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="home" href="/home">
+            <IonIcon icon={todayOutline} />
+            <IonLabel>Today</IonLabel>
+          </IonTabButton>
+
+          <IonTabButton tab="tasks" href="/tasks">
+            <IonIcon icon={calendarNumberOutline} />
+            <IonLabel>Calendário</IonLabel>
+          </IonTabButton>
+          
+
+          <IonTabButton tab="details" href="/details">
+            <IonIcon icon={personOutline} />
+            <IonLabel>Details</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+
+      </IonTabs>
     </IonReactRouter>
   </IonApp>
 );
