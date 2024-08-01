1ada1457c51af24c6ab28dc78c12c483098c6a53
augustofrade <aquaaticnaotrava@gmail.com>
2024-08-01 18:51:12 -0300

    add home task list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,63 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonReorder, IonReorderGroup, IonTitle, IonToolbar, ItemReorderEventDetail } from '@ionic/react';
 import './Home.css';
+import { useState } from 'react';
+import { Task } from '../types/interfaces';
 
 const Home: React.FC = () => {
+
+  // TODO: fetch tasks from storage
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      title: "1 - Do the dishes",
+      description: "Do the dishes asap b4 family gets home"
+    },
+    {
+      title: "2 - Create a pixel art",
+      description: "Create a pixel art and sent to friend"
+    },
+    {
+      title: "3 - Develop app",
+      description: "Develop the tasks app"
+    }
+  ]);
+
+  function handleReorder(event: CustomEvent<ItemReorderEventDetail>) {
+    setTasks(event.detail.complete(tasks));
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    event.detail.complete();
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Today</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Today</IonTitle>
           </IonToolbar>
         </IonHeader>
 
-
+        <IonList>
+          {/* The reorder gesture is disabled by default, enable it to drag and drop items */}
+          <IonReorderGroup disabled={false} onIonItemReorder={handleReorder}>
+            {
+              // TODO: change key to task ID
+              tasks.map(task => (
+                <IonItem key={task.title}>
+                  <IonLabel>{ task.title }</IonLabel>
+                  <IonReorder slot="end"></IonReorder>
+                </IonItem>
+              ))
+            }
+          </IonReorderGroup>
+        </IonList>
 
       </IonContent>
     </IonPage>
