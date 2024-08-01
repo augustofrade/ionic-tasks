77841c53c7c132430dfac9b8a5fc426f073c4fb2
augustofrade <aquaaticnaotrava@gmail.com>
2024-08-01 15:38:42 -0300

    Add scaffold

---
+++ src/pages/TaskCreation/index.tsx
@@ -0,0 +1,14 @@
+import './index.css';
+
+import { IonPage } from '@ionic/react';
+import React from 'react';
+
+interface Props { }
+
+const TaskCreation: React.FC<Props> = () => {
+    return (
+        <IonPage>
+            
+        </IonPage>
+    )
+}
