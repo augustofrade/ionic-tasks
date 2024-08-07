4700bff46362f63c456b684a3dd19d392243c1e0
augustofrade <aquaaticnaotrava@gmail.com>
2024-08-06 21:17:38 -0300

    Revamped TaskDetailsPage

--- src/pages/TaskDetailsPage.tsx
+++ src/pages/TaskDetailsPage.tsx
@@ -1,39 +1,156 @@
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSkeletonText } from "@ionic/react"
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSkeletonText, IonText, useIonViewWillEnter, useIonViewDidEnter, useIonViewDidLeave, useIonViewWillLeave, IonList, IonItem, IonIcon, IonLabel, IonBackButton, IonButtons, IonButton, IonPopover, IonChip } from "@ionic/react"
 import { useEffect, useState } from "react";
 import { SavedTask } from "../types/interfaces";
 import { StorageHandler } from "../api/StorageHandler";
 import { RouteComponentProps } from "react-router";
+import DatePicker from "../components/DatePicker";
+import TimePicker from "../components/TimePicker";
+import { alarmOutline, calendarOutline, ellipsisVertical, flagOutline, pencilOutline, trashBinOutline } from "ionicons/icons";
+import dayjs from "dayjs";
+import { Priority } from "../types/enums";
 
 interface TaskDetailsPageProps extends RouteComponentProps<{
 	id: string;
 }> {}
 
+const SkeletonText: React.FC<{ height?: number}> = ({ height }) => {
+	return (
+		<IonSkeletonText animated={true} style={{ height: height ?? 15 }} />
+	)
+}
+
 const TaskDetailsPage: React.FC<TaskDetailsPageProps> = ({ match }) => {
 
 	const [info, setInfo] = useState<SavedTask | null>(null);
-	useEffect(() => {
 
+	const dateLabel = dayjs().isSame(info?.date, "day")
+		? "Today"
+		: dayjs(info?.date).format("ddd, MMM D, YYYY") ;
+
+	const reminderLabel = info?.reminder ? dayjs(info.date).format("HH:mm") : "None";
+
+	// TODO: set priority display colors
+	const priorityLabel = info?.priority
+		? Priority[info.priority as unknown as keyof typeof Priority]
+		: "Not defined";
+	
+	useIonViewDidEnter(() => {
 		StorageHandler.instance().get(match.params.id)
-		.then(res => setInfo(res))
+		.then(res => {
+			res.labels = ["Work", "Study", "Gym", "Family", "Groceries", "Programming Stuff An"];
+			setInfo(res);
+		})
 		.catch(err => console.log(err));
 		// TODO: show error warning
-	}, []);
+	});
+
+	useIonViewDidLeave(() => {
+		setInfo(null);
+	});
 
 	return (
 		<IonPage>
 			<IonHeader className="ion-no-border">
 				<IonToolbar>
-					<IonTitle>
+					<IonButtons slot="start">
+						<IonBackButton />
+					</IonButtons>
+
+					<IonTitle className="ion-text-center">
 						{ info?.title }
 					</IonTitle>
+					
+					<IonButtons slot="end">
+						<IonButton shape="round" id="task-options">
+							<IonIcon icon={ellipsisVertical} />
+						</IonButton>
+						<IonPopover trigger="task-options">
+							<IonContent>
+								<IonList>
+									<IonItem lines="none">
+										<IonIcon icon={trashBinOutline} slot="start" />
+										<IonLabel>Delete</IonLabel>
+									</IonItem>
+									<IonItem lines="none">
+										<IonIcon icon={pencilOutline} slot="start" />
+										<IonLabel>Edit</IonLabel>
+									</IonItem>
+								</IonList>
+							</IonContent>
+						</IonPopover>
+					</IonButtons>
 				</IonToolbar>
 			</IonHeader>
-			<IonContent fullscreen>
-				<IonHeader collapse="condense">
-					<IonToolbar>
-						<IonTitle size="large">{ info?.title }</IonTitle>
-					</IonToolbar>
-				</IonHeader>
+			<IonContent fullscreen className="ion-padding">
+				{
+					info
+					? <>
+					<IonHeader collapse="condense">
+						<IonToolbar>
+							<IonTitle>{ info?.title }</IonTitle>
+						</IonToolbar>
+					</IonHeader>
+					<div>
+						<IonList className="ion-margin-vertical">
+							<IonItem lines="full">
+								<IonIcon icon={calendarOutline} slot="start" />
+								<IonLabel style={{ padding: "8px 0" }}>{dateLabel}</IonLabel>
+							</IonItem>
+							<IonItem lines="full" disabled={!info.reminder}>
+								<IonIcon icon={alarmOutline} slot="start" />
+								<IonLabel style={{ padding: "8px 0" }}>Reminder: {reminderLabel}</IonLabel>
+							</IonItem>
+							<IonItem lines="none">
+								<IonIcon icon={flagOutline} slot="start" />
+								<IonLabel style={{ padding: "8px 0" }}>{ priorityLabel }</IonLabel>
+							</IonItem>
+						</IonList>
+
+						<div className="ion-text-center">
+							{
+								info?.labels?.map(label => (
+									<IonChip className="ion-text-center" style={{ flex: 1 }}>{label}</IonChip>
+								))
+							}
+						</div>
+
+						<div className="ion-margin-top">
+							<IonText style={{ textAlign: "justify", display: "block" }}>{ info?.description }</IonText>
+						</div>
+					</div>
+					</>
+					// TODO: create component for the whole page + skeleton text
+					: <div>
+						<SkeletonText height={25} />
+						<IonList className="ion-margin-vertical">
+							<IonItem lines="full">
+								<IonIcon icon={calendarOutline} slot="start" />
+								<IonLabel style={{ padding: "8px 0" }}>
+									<SkeletonText />
+								</IonLabel>
+							</IonItem>
+							<IonItem lines="full">
+								<IonIcon icon={alarmOutline} slot="start" />
+								<IonLabel style={{ padding: "8px 0" }}>
+									<SkeletonText />
+								</IonLabel>
+							</IonItem>
+							<IonItem lines="none">
+								<IonIcon icon={flagOutline} slot="start" />
+								<IonLabel style={{ padding: "8px 0" }}>
+									<SkeletonText />
+								</IonLabel>
+							</IonItem>
+						</IonList>
+
+						<div className="ion-margin-top">
+							<IonText style={{ textAlign: "justify", display: "block" }}>
+								<SkeletonText height={100} />
+							</IonText>
+						</div>
+					</div>
+					
+				}
 			</IonContent>
 		</IonPage>
 	)
